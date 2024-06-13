70cdeb2d7833b5d5ee600d7965f432f8db9907c2
Pritam <manoharipritam@gmail.com>
2024-06-13 10:08:00 +0530

    UI complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom"
+import LoginPage from "./pages/auth/LoginPage"
+import SignUpPage from "./pages/auth/SignUpPage"
+import HomePage from "./pages/home/HomePage"
+
 function App() {
   return (
-    <>
-      Hello World
-    </>
+    <Router>
+      <div className='flex max-w-6xl mx-auto'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
